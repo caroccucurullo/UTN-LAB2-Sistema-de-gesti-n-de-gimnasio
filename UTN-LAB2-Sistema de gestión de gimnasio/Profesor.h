6fea85028bb73c57bc596cb0ff9fedcbe6f098d8
B2DF <bernardinarte@gmail.com>
2022-11-17 01:17:34 -0300

    turno fix

--- UTN-LAB2-Sistema de gestión de gimnasio/Profesor.h
+++ UTN-LAB2-Sistema de gestión de gimnasio/Profesor.h
@@ -17,23 +17,23 @@ class Profesor : public Persona {
 private:
     int _id;
     int _idDisciplina;
-    int _turnoId;
+    char _turno[20];
     Fecha _fIngreso;
     Fecha _fEgreso;
     float _sueldo;
 
 public:
-    Profesor(int id = 0, int idDis = 0, int turnoId = 0, Fecha fIn = Fecha(), float s = 0);
+    Profesor(int id = 0, int idDis = 0, std::string turno = "", Fecha fIn = Fecha(), float s = 0);
     int getId() { return _id; }
     int getIdDisciplina() { return _idDisciplina; }
-    int getTurnoId() { return _turnoId; }
+    std::string getTurno() { std::string turno(_turno); return turno; }
 	Fecha getFechaIngreso() { return _fIngreso; }
 	Fecha getFechaEgreso() { return _fEgreso; }
     float getSueldo() { return _sueldo; }
 	
     void setId(int id) { _id = id; }
     void setIdDisciplina(int id) { _idDisciplina = id; }
-    void setTurnoId(int turnoId) { _turnoId = turnoId; }
+	void setTurno(std::string turno) { strcpy(_turno, turno.c_str()); }
 	void setFechaIngreso(Fecha fi) { _fIngreso = fi; }
     void setFechaEgreso(Fecha fe) { _fEgreso = fe; }
     void setSueldo(float s) { _sueldo = s; }
--- UTN-LAB2-Sistema de gestión de gimnasio/Rol.h
+++ UTN-LAB2-Sistema de gestión de gimnasio/Rol.h
@@ -15,22 +15,22 @@ private:
     int _id;
     char _cargo[30];
     float _sueldo;
-    int _turnoId;
+    char _turno[20];
 	Fecha _fechaIngreso;
 	Fecha _fechaEgreso;
 public:
-    Rol(int id = 0, std::string cargo="", float sueldo = 0, int turnoId = 0, Fecha fechaIngreso=Fecha());
+    Rol(int id = 0, std::string cargo="", float sueldo = 0, std::string turno="", Fecha fechaIngreso=Fecha());
     int getId() { return _id; }
     std::string getCargo() { std::string cargo(_cargo); return cargo; }
     float getSueldo() { return _sueldo; }
-    int getTurno() { return _turnoId; }
+    std::string getTurno() { std::string turno(_turno); return turno; }
 	Fecha getFechaIngreso() { return _fechaIngreso; }
 	Fecha getFechaEgreso() { return _fechaEgreso; }
 	
     void setId(int id) { _id=id; }
     void setCargo(std::string cargo) { strcpy(_cargo, cargo.c_str()); }
     void setSueldo(float s) { _sueldo = s; }
-	void setTurno(int turno) { _turnoId = turno; }
+	void setTurno(std::string turno) { strcpy(_turno, turno.c_str()); }
 	void setFechaIngreso(Fecha fechaIngreso) { _fechaIngreso = fechaIngreso; }
 	void setFechaEgreso(Fecha fechaEgreso) { _fechaEgreso = fechaEgreso; }
 
